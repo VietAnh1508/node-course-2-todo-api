20568c87ecf33149e34d4cfc3ac4677bd2c30108
Viet Anh <vietanhtong1508@gmail.com>
2017-10-29 18:53:28 +0700

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -3,11 +3,11 @@ require('./config/config');
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
-const {ObjectID} = require('mongodb');
+const { ObjectID } = require('mongodb');
 
-let {mongoose} = require('./db/mongoose');
-let {Todo} = require('./models/todo');
-let {User} = require('./models/user');
+let { mongoose } = require('./db/mongoose');
+let { Todo } = require('./models/todo');
+let { User } = require('./models/user');
 
 const app = express();
 app.use(bodyParser.json());
@@ -19,19 +19,16 @@ app.post('/todos', (req, res) => {
     let todo = new Todo({
         text: req.body.text
     });
+
     todo.save().then(doc => {
         res.send(doc);
-    }, e => {
-        res.status(400).send(e);
-    });
+    }, e => res.status(400).send(e));
 });
 
 app.get('/todos', (req, res) => {
     Todo.find().then(todos => {
-        res.send({todos});
-    }, e => {
-        res.status(400).send(e);
-    });
+        res.send({ todos });
+    }, e => res.status(400).send(e));
 });
 
 app.get('/todos/:id', (req, res) => {
@@ -42,7 +39,7 @@ app.get('/todos/:id', (req, res) => {
 
     Todo.findById(id).then(todo => {
         if (todo) {
-            return res.send({todo});
+            return res.send({ todo });
         }
         res.status(404).send();
     }).catch(e => res.status(400).send());
@@ -56,7 +53,7 @@ app.delete('/todos/:id', (req, res) => {
 
     Todo.findByIdAndRemove(id).then(todo => {
         if (todo) {
-            return res.send({todo});
+            return res.send({ todo });
         }
         res.status(404).send();
     }).catch(e => res.status(404).send());
@@ -77,12 +74,22 @@ app.patch('/todos/:id', (req, res) => {
         body.completedAt = null;
     }
 
-    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then(todo => {
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then(todo => {
         if (!todo) {
             return res.status(404).send();
         }
-        res.send({todo});
+        res.send({ todo });
     }).catch(e => res.status(400).send());
-})
+});
 
-module.exports = {app};
+app.post('/users', (req, res) => {
+    let body = _.pick(req.body, ['email', 'password']);
+    let user = new User(body);
+
+    user.save()
+        .then(() => user.generateAuthToken())
+        .then(token => res.header('x-auth', token).send(user))
+        .catch(e => res.status(400).send(e));
+});
+
+module.exports = { app };
